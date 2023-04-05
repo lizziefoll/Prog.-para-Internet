199cbfb0f64314bc2b2c022f4e95ee6958a1e7a7
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-04-05 11:58:48 -0300

    Create listform.js

---
+++ siteList/listform.js
@@ -0,0 +1,34 @@
+const namelist = ["Aline", "Otavio", "Camila","Elisabeth"]
+
+const listE1 = document.getElementById("list");
+const searchField = document.getElementById("searchField");
+searchField.addEventListener("input", inputHandler);
+
+this.fillList();
+
+function fillList(list = namelist) {
+    listE1.innerHTML = "";
+    for(let i=0; i < list.length; i++) {
+        let listItems = document.createElement("li");
+        listItems.innerHTML = list[i];
+        listE1.appendChild(listItems);
+    }
+}
+
+function inputHandler() {
+    const filteredList = namelist.filter(el => {
+        const listItems = el.toLowerCase();
+        const searchWord = searchField.value.toLowerCase();
+        return listItems.includes(searchWord)
+    });
+    fillList(filteredList);
+}
+
+
+function Adicionar(){
+  
+}
+
+function Selecionar(){
+
+}
