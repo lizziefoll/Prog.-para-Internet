3533a940432f0561eb1c7d79748d26e085966399
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-06-21 11:56:50 -0300

    Create romanos.js

---
+++ romanos/romanos.js
@@ -0,0 +1,23 @@
+//criar referencias ao elementos da pagina 
+const frm = document.querySelector("form"); 
+const resp = document.querySelector("pre"); 
+
+frm.addEventListener("submit", (e) => {
+    //evita o envio do formulário 
+    e.preventDefault(); 
+
+    const num = Number(frm.inNumero.value); 
+
+    let num1 = "I";
+    let num5 = "V";
+    let num10 = "X";
+    let num50 = "L";
+    let num100 = "C";
+    let num500 = "D";
+    let num1000 = "M";
+
+    
+    
+
+
+})
